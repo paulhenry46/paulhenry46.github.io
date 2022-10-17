2181452f8620f94b4ac1d1449d2c28e2e6fa6411
Paulhenry <89080931+paulhenry46@users.noreply.github.com>
2022-10-17 20:40:33 +0200

    retour à la normale

--- assets/admin/scripts.js
+++ assets/admin/scripts.js
@@ -1,4 +1,4 @@
-async function sampleFunction() {
+function sampleFunction() {
   var titredesfichiers = document.getElementById("titredesfichiers").value;
   var description = document.getElementById("age").value;
   var nom = document.getElementById("nom").value;
@@ -15,10 +15,6 @@ async function sampleFunction() {
   var mat8 = "\">";
   var mat81 = "</a>\n \t \t \t \t \t \t \t \t </div>\n \t \t \t \t \t \t \t </div>\n \t \t \t \t \t \t \t <div class=\"col-lg-6 col-md-8\">\n \t \t \t \t \t \t \t <a href=\"assets/";
   var mat9 = ".pdf\" class=\"btn btn-primary btn-sm btn-block animate-icon\">PDF<i class=\"ml-1 no-mr zmdi zmdi-long-arrow-right\"></i></a>\n \t \t \t \t \t \t</div>\n \t \t \t \t \t</div>\n \t \t \t \t</div>\n \t \t \t \t </article>\n \t \t \t </div>\n \t \t \t <!-- Fin article-->";
-  var mat9c = "vn.pdf\" class=\"btn btn-primary btn-sm btn-block animate-icon\">NOIR<i class=\"ml-1 no-mr zmdi zmdi-long-arrow-right\"></i></a>";
-  var mat9c2 = "<a href=\"assets/";
-  var mat9c22 = "vc.pdf\" class=\"btn btn-primary btn-sm btn-block animate-icon\">CLAIR<i class=\"ml-1 no-mr zmdi zmdi-long-arrow-right\"></i></a>";
-  var mat9c3 = "\n \t \t \t \t \t \t</div>\n \t \t \t \t \t</div>\n \t \t \t \t</div>\n \t \t \t \t </article>\n \t \t \t </div>\n \t \t \t <!-- Fin article-->";
   var nouv1 = "\t \t \t <!--D\u00e9but article-->\n \t \t \t <div class=\"col-lg-4 col-md-6 masonry-item wow\">\n \t \t \t \t <article class=\"card card-";
   var nouv2 = " mb-4 \">\n \t \t \t \t \t <figure class=\"ms-thumbnail ms-thumbnail-left\">\n \t \t \t \t \t \t <img id=\"";
   var nouv21 = "\" src=\"assets/";
@@ -82,21 +78,16 @@ async function sampleFunction() {
   if(document.getElementById('fiche').checked == true){
     var type = "Fiche de révison"
     var couleurmat = "info"
-    var finalmat = mat1+couleurmat+mat2+ide+mat21+repertoire1+repertoire2+titredesfichiers+mat3+repertoire1+repertoire2+titredesfichiers+mat4+nom+mat6+description+mat7+couleurmat+mat8+type+mat81+repertoire1+repertoire2+titredesfichiers+mat9
-    var finalnouv = nouv1+couleurnouv+nouv2+ide+nouv21+repertoire1+repertoire2+titredesfichiers+nouv3+repertoire1+repertoire2+titredesfichiers+nouv4+matiere+nouv5+nom+nouv6+description+nouv7+couleurnouv+nouv9+matiere+nouv10+couleurnouv+nouv11+date+nouv12+repertoire1+repertoire2+titredesfichiers+nouv13
   }
   if(document.getElementById('carte').checked == true){
     var type = "Carte mentale"
     var couleurmat = "success"
-    var finalmat = mat1+couleurmat+mat2+ide+mat21+repertoire1+repertoire2+titredesfichiers+mat3+repertoire1+repertoire2+titredesfichiers+mat4+nom+mat6+description+mat7+couleurmat+mat8+type+mat81+repertoire1+repertoire2+titredesfichiers+mat9c+mat9c2+repertoire1+repertoire2+titredesfichiers+mat9c22+mat9c3
-    var finalnouv = nouv1+couleurnouv+nouv2+ide+nouv21+repertoire1+repertoire2+titredesfichiers+nouv3+repertoire1+repertoire2+titredesfichiers+nouv4+matiere+nouv5+nom+nouv6+description+nouv7+couleurnouv+nouv9+matiere+nouv10+couleurnouv+nouv11+date+nouv12+repertoire1+repertoire2+titredesfichiers+nouv13
   }
   if(document.getElementById('methodo').checked == true){
     var type = "Méthodologie"
     var couleurmat = "royal"
-    var finalmat = mat1+couleurmat+mat2+ide+mat21+repertoire1+repertoire2+titredesfichiers+mat3+repertoire1+repertoire2+titredesfichiers+mat4+nom+mat6+description+mat7+couleurmat+mat8+type+mat81+repertoire1+repertoire2+titredesfichiers+mat9
-    var finalnouv = nouv1+couleurnouv+nouv2+ide+nouv21+repertoire1+repertoire2+titredesfichiers+nouv3+repertoire1+repertoire2+titredesfichiers+nouv4+matiere+nouv5+nom+nouv6+description+nouv7+couleurnouv+nouv9+matiere+nouv10+couleurnouv+nouv11+date+nouv12+repertoire1+repertoire2+titredesfichiers+nouv13
   }
+  var final = description+nom+titredesfichiers+Public+repertoire2+matiere+date+type;
   var finalmat = mat1+couleurmat+mat2+ide+mat21+repertoire1+repertoire2+titredesfichiers+mat3+repertoire1+repertoire2+titredesfichiers+mat4+nom+mat6+description+mat7+couleurmat+mat8+type+mat81+repertoire1+repertoire2+titredesfichiers+mat9
   var finalnouv = nouv1+couleurnouv+nouv2+ide+nouv21+repertoire1+repertoire2+titredesfichiers+nouv3+repertoire1+repertoire2+titredesfichiers+nouv4+matiere+nouv5+nom+nouv6+description+nouv7+couleurnouv+nouv9+matiere+nouv10+couleurnouv+nouv11+date+nouv12+repertoire1+repertoire2+titredesfichiers+nouv13
   document.getElementById("sampleText").value = final;
